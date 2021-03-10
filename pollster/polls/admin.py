bbde8145bb20f913e9fecbd0e0b8e4267a2c42cd
Shadee Merhi <shadmerhi@gmail.com>
2021-03-09 21:53:53 -0700

    create base models and admin page

---
+++ pollster/polls/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question, Choice
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
